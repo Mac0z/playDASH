1fb5f2a39273d0ada00a48b4a2f083522dca6166
Mac0z <odw199@zepler.net>
2015-02-22 21:39:40 +0800

    version 1.2

--- Bin/playdash.sh
+++ Bin/playdash.sh
@@ -1,20 +1,8 @@
 #!/bin/sh
-#wrapper for vlc
 scriptpid=$$
 apppid=/tmp/.playvlc-app.$$.pid
 app=/Applications/VLC.app/Contents/MacOS/VLC
 
 echo Streaming: Launching $app "$@" >&2
-(
-    "$app" "$@"  &
-
-    echo $! > $apppid
-    wait
-    rm -f $apppid
-    kill $scriptpid 2> /dev/null
-) < /dev/null &
-
-cat > /dev/null
-if [ -s $apppid ] ; then
-    kill `cat $apppid` 2> /dev/null
-fi
+"$app" "$@"  &
+wait $!
